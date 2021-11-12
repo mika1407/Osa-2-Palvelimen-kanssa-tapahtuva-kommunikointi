19e6221dd476864fcf80addb6c2dd0003588eaa9
mika tiihonen <koneautomaatio@hotmail.com>
2021-11-12 19:19:13 +0200

    2.6: puhelinluettelo step1

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
-import React from 'react'
-import Course from './components/Course'
+import React, { useState } from 'react'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+    const addName = (event) => {
+      event.preventDefault()
+      const nameObject = {
+          name: newName,
       }
-    ]
+      setPersons([...persons,nameObject])
+  }
+
+  const handleNameChange = (event) => {
+      setNewName(event.target.value)
   }
 
   return (
     <div>
-      <Course course={course} />
+      <h2>Phonebook</h2>
+      <form onSubmit={addName} >
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+        {persons.map(person => (
+            <p key={person.name}>{person.name}</p>
+        ))}
     </div>
   )
-}
 
+}
 
 export default App
