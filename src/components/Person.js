97b9bb7a06798ab3b5dc01dc4aad317a8210a659
mika tiihonen <koneautomaatio@hotmail.com>
2022-03-22 19:28:09 +0200

    2.8: puhelinluettelo step3

---
+++ src/components/Person.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Person = (props) =>
+    props.persons.map(person => (
+        <p key={person.name}>{person.name} {person.number}</p>
+        ))
+
+export default Person;
